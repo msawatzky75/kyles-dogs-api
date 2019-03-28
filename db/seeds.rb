371fd9813624746ce38003834a2adc6c76b3b570
Spiffpitt <34319870+spiffpitt@users.noreply.github.com>
2019-03-28 10:46:36 -0500

    Added seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+AdminUser.destroy_all if Rails.env.development?
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+Product.destroy_all
+ProductCategory.destroy_all
+
+normal = ProductStatusCode.create(name: 'Normal', description: 'This marks a normal product.')
+new = ProductStatusCode.create(name: 'New', description: 'This marks a new product.')
+sale = ProductStatusCode.create(name: 'Sale', description: 'This marks a product on sale.')
+
+standard = ProductCategory.create(name: "Standard", description: "The standard mealdog category.")
+# .products.create(name: "The Standard", description: "The standard mealdog.", price: 9.99, product_status_code: normal)
+
+standard.products.create(name: Spicy::Proton.format('%b %v').titleize + ' Mealdog', description: 'A ' + Spicy::Proton.adjective + 'tasting mealdog.')
