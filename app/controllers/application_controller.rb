a44ffdb7497db2abb81e69a97717539c338afc95
Spiffpitt <34319870+spiffpitt@users.noreply.github.com>
2019-04-11 11:20:50 -0500

    Added redirects for built SPA

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
-# frozen_string_literal: true
-
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  rescue_from ActionController::RoutingError, with: :not_found
+
+  def raise_not_found
+    raise ActionController::RoutingError.new("No route matches #{params[:unmatched_route]}")
+  end
+
+  def not_found
+    respond_to do |format|
+      format.html { render file: "#{Rails.root}/public/index.html", layout: false, status: :not_found }
+      format.xml { head :not_found }
+      format.any { head :not_found }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,5 @@ Rails.application.routes.draw do
     resources :product_categories
     resources :product_status_codes
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'admin/dashboard#index'
+  get '*unmatched_route', to: 'application#raise_not_found'
 end
